18eea829edcb29e61f22e44fdca890889db0ac08
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2024-10-17 17:43:17 +0530

    Create variable.tf

---
+++ Terraform/modules/variable.tf
@@ -0,0 +1,38 @@
+variable "vpc_cidr_block" {
+    type = string
+  
+}
+
+
+variable "aws_subnet" {
+    type = list(string)
+  
+}
+
+variable "az" {
+    type = list(string)
+  
+}
+
+variable "public_ip" {
+  
+  type = bool
+}
+
+variable "port_no" {
+  type = list(number)
+}
+
+variable "ami_id" {
+    type = string
+  
+}
+
+variable "instance_type" {
+    type = string
+  
+}
+
+variable "key" {
+  type = string
+}
