6aad481a60d70d8043df205b8c9ac341fe9e40a5
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2024-10-17 17:42:27 +0530

    Create main.tf

---
+++ Terraform/modules/main.tf
@@ -0,0 +1,21 @@
+module "vpc" {
+    source = "./modules/vpc"
+    vpc_cidr_block = var.vpc_cidr_block
+    aws_subnet = var.aws_subnet
+    az = var.az
+    public_ip = var.public_ip
+    port_no = var.port_no
+}
+
+
+
+
+
+
+
+module "ec2" {
+    source = "./modules/ec2"
+    ami_id = var.ami_id
+    instance_type = var.instance_type
+    key = var.key
+}
